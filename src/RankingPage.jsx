982a4175f26cf9da38f4caa0e3e855553ff5381b
Tomasz Pewiński <pewniak747@gmail.com>
2017-01-18 18:17:59 +0100

    Ranking page stump

---
+++ src/RankingPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RankingPage extends Component {
+  render() {
+    return (
+      <div>
+        TODO: ranking
+      </div>
+    );
+  }
+}
+
+export default RankingPage;
