aa85bf06a6bb276284088caa668653f24dd021a4
Tomasz Pewiński <pewniak747@gmail.com>
2017-01-18 21:38:37 +0100

    Remove boilerplate code from generator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Router, Route, browserHistory } from 'react-router';
 
 import routes from './routes';
@@ -11,11 +9,7 @@ import RepoPage from './RepoPage';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+      <div>
         <Router history={browserHistory}>
           <Route path={routes.ranking} component={RankingPage} />
           <Route path={routes.user(':id')} component={UserPage} />
