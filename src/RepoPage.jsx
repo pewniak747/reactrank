8e08fe044a3538d7f96f789be12df0eeb0520a0a
Tomasz Pewiński <pewniak747@gmail.com>
2017-01-19 21:24:33 +0100

    Style repo page

--- src/RepoPage.jsx
+++ src/RepoPage.jsx
@@ -7,43 +7,44 @@ import {
 } from './store';
 import routes from './routes';
 
+import Octicon from './Octicon';
+import styles from './RepoPage.css';
+
 const UserContribution = ({ id, contributions }) => {
   const user = getUserById(id);
 
   return (
-    <div>
-      <dt>User</dt>
-      <dd>
-        <Link to={routes.user(id)}>
-          {user.login}
-        </Link>
-      </dd>
-      <dt>Contributions</dt>
-      <dd>{contributions}</dd>
-    </div>
+    <Link to={routes.user(id)} className={styles.user}>
+      <img src={user.avatarUrl} className={styles.userAvatar} alt={user.login} />
+      <div className={styles.userLogin}>
+        {user.login}
+      </div>
+      <div className={styles.userStat}>
+        {contributions}
+        <Octicon className={styles.userStatIcon} name="git-commit" />
+      </div>
+    </Link>
   );
 }
 
 export default ({ params: { id } }) => {
   const repo = getRepoById(id);
+  const url = `https://github.com/${repo.fullName}`
 
   return (
-    <div>
-      Repo:
-      <dl>
-        <dt>ID</dt>
-        <dd>{repo.id}</dd>
-        <dt>Name</dt>
-        <dd>{repo.name}</dd>
-        <dt>Full name</dt>
-        <dd>{repo.fullName}</dd>
-        <dt>Stars</dt>
-        <dd>{repo.stars}</dd>
-      </dl>
-      Contributions:
-      {repo.users.map(({ id, contributions }) =>
-        <UserContribution key={id} id={id} contributions={contributions} />
-      )}
+    <div className={styles.root}>
+      <div className={styles.header}>
+        <a href={url} target="_blank" className={styles.link}>
+          <Octicon name="mark-github" />
+          &nbsp;
+          {repo.fullName}
+        </a>
+      </div>
+      <div className={styles.users}>
+        {repo.users.map(({ id, contributions }) =>
+          <UserContribution key={id} id={id} contributions={contributions} />
+        )}
+      </div>
     </div>
   );
 };
